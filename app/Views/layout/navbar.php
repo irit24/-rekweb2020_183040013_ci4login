27d80171dcd7ba2f1c7977e40f77fd9b8e312c11
irit24 <tadzaka.10@gmail.com>
2020-11-09 19:57:59 +0700

    First Commit - Login with Myth/Auth

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -24,6 +24,12 @@
                     <a class="nav-link" href="/orang">Orang</a>
                 </li>
             </ul>
+            <?php if (logged_in()) : ?>
+                <a class="nav-link" href="/logout">Logout</a>
+            <?php else : ?>
+                <a class="nav-link" href="/login">Login</a>
+            <?php endif; ?>
+
             <!-- <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
